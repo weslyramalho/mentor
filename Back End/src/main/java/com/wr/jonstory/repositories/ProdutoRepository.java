138121af96355cd1c5b664a3a0e91c423b8cba53
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-06 18:08:39 -0300

    Foi criado a interface ProdutoRepository e  implementado suas extensões

---
+++ Back End/src/main/java/com/wr/jonstory/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.wr.jonstory.repositories;
+
+import com.wr.jonstory.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
