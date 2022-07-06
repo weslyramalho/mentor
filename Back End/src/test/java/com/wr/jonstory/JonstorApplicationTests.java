7e20b52006fc1b9d85a9ab07c1611a3a91a0e529
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-06 15:08:03 -0300

    Projeto inicializado

---
+++ Back End/src/test/java/com/wr/jonstory/JonstorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wr.jonstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonstorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
