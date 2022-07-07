7c503025200e9e0ea48afbd27487ef6408a564f6
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-07 19:33:19 -0300

    Foi implementado a interface PedidoRepository

---
+++ Back End/src/main/java/com/wr/jonstory/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.wr.jonstory.repositories;
+
+import com.wr.jonstory.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+}
