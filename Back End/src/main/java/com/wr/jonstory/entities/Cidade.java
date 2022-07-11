57fca617a0c23fdc235d32f7b4fe2b966bfd1484
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-11 08:12:28 -0300

    Foi criado a classe Cidade

---
+++ Back End/src/main/java/com/wr/jonstory/entities/Cidade.java
@@ -0,0 +1,30 @@
+package com.wr.jonstory.entities;
+
+public class Cidade {
+    private Integer id;
+    private String nome;
+
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
