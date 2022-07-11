9f8dc8654b891ea470f5e8c6916230c698eb7381
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-11 08:20:49 -0300

    foi criado a classe endereço

---
+++ Back End/src/main/java/com/wr/jonstory/entities/Endereco.java
@@ -0,0 +1,70 @@
+package com.wr.jonstory.entities;
+
+public class Endereco {
+    private Integer id;
+    private String logradouro;
+    private String nome;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    public Endereco() {
+    }
+
+    public Endereco(Integer id, String logradouro, String nome, String complemento, String bairro, String cep) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.nome = nome;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
