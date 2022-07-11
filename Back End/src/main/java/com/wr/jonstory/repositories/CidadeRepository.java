d6bf51453fe691e5fdcb90fa76bacefc4a1508ed
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-11 08:15:22 -0300

    Foi criado a Interface CidadeRepository

---
+++ Back End/src/main/java/com/wr/jonstory/repositories/CidadeRepository.java
@@ -0,0 +1,8 @@
+package com.wr.jonstory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository {
+}
