c46d49578035763f9d7e2c9a04e2c7c34ebb62ad
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-11 08:03:25 -0300

    Foi criado a classe estado

---
+++ Back End/src/main/java/com/wr/jonstory/entities/Estado.java
@@ -0,0 +1,30 @@
+package com.wr.jonstory.entities;
+
+public class Estado {
+    private Integer id;
+    private String nome;
+
+    public Estado() {
+    }
+
+    public Estado(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
