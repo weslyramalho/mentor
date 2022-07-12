d1dd0c6a7f1e4b24b149015f2edfe471ba56fa38
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-12 20:19:52 -0300

    Foi criado a classe ItemPedido

---
+++ Back End/src/main/java/com/wr/jonstory/entities/ItemPedido.java
@@ -0,0 +1,58 @@
+package com.wr.jonstory.entities;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class ItemPedido {
+    @EmbeddedId
+    private ItemPedidoPk id = new ItemPedidoPk();
+
+    private Double desconto;
+    private Integer quantidade;
+    private Double preco;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(ItemPedidoPk id, Double desconto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public ItemPedidoPk getId() {
+        return id;
+    }
+
+    public void setId(ItemPedidoPk id) {
+        this.id = id;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
