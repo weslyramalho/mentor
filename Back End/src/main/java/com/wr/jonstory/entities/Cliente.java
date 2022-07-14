bee10b9f64ebaab231d21110e4ea7f4fdaa2c60a
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-13 21:03:28 -0300

    Foi implementado o serializable na classe cliente

--- Back End/src/main/java/com/wr/jonstory/entities/Cliente.java
+++ Back End/src/main/java/com/wr/jonstory/entities/Cliente.java
@@ -2,8 +2,16 @@ package com.wr.jonstory.entities;
 
 import com.wr.jonstory.entities.enums.TipoCliente;
 
-public class Cliente {
-    private Integer id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Entity
+public class Cliente implements Serializable {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Integer id;
     private String nome;
     private String email;
     private String cpfoucnpj;
