0b4ed0a8a766d8ee38ea14756b57a3695ff9e076
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-20 19:59:12 -0300

    Criado EstadoRepository

---
+++ Back End/src/main/java/com/wr/jonstory/services/EstadoService.java
@@ -0,0 +1,21 @@
+package com.wr.jonstory.services;
+
+import com.wr.jonstory.entities.Endereco;
+import com.wr.jonstory.entities.Estado;
+import com.wr.jonstory.repositories.EstadoRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstadoService {
+
+    @Autowired
+    private EstadoRespository respository;
+
+    public List<Estado> findAll(){
+        return repository.findAll();
+    }
+
+}
