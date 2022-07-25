b6cfecb4fd9c96eef39ab6f8df654373fb372509
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-25 19:46:02 -0300

    CidadeController

---
+++ Back End/src/main/java/com/wr/jonstory/controllers/CidadeController.java
@@ -0,0 +1,9 @@
+package com.wr.jonstory.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cidade")
+public class CidadeController {
+}
