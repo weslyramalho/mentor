a05cf4733d6ba8260b5576bc4bd9c677210b8e83
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-26 16:06:05 -0300

    Foi criado a EstadoPagamento

--- Back End/src/main/java/com/wr/jonstory/entities/Pedido.java
+++ Back End/src/main/java/com/wr/jonstory/entities/Pedido.java
@@ -13,13 +13,18 @@ public class Pedido implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Date instante;
-
+    private Pagamento pagamento;
+    private Cliente cliente;
+    private Endereco enderecoDeEntrega;
     public Pedido() {
     }
 
-    public Pedido(Integer id, Date instante) {
+    public Pedido(Integer id, Date instante, Pagamento pagamento, Cliente cliente, Endereco enderecoDeEntrega) {
         this.id = id;
         this.instante = instante;
+        this.pagamento = pagamento;
+        this.cliente = cliente;
+        this.enderecoDeEntrega = enderecoDeEntrega;
     }
 
     public Integer getId() {
@@ -36,5 +41,29 @@ public class Pedido implements Serializable {
 
     public void setInstante(Date instante) {
         this.instante = instante;
+    }
+
+    public Pagamento getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(Pagamento pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Endereco getEnderecoDeEntrega() {
+        return enderecoDeEntrega;
+    }
+
+    public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+        this.enderecoDeEntrega = enderecoDeEntrega;
     }
 }
---
+++ Back End/src/main/java/com/wr/jonstory/entities/enums/EstadoPagamento.java
@@ -0,0 +1,23 @@
+package com.wr.jonstory.entities.enums;
+
+public enum EstadoPagamento {
+
+    PENDENTE(1, "Pendente"),
+    QUITADO(2, "Quitado"),
+    CANCELADO(3, "Cancelado");
+
+    private int cod;
+    private String descricao;
+    EstadoPagamento(int cod, String descricao) {
+        this.cod = cod;
+        this.descricao = descricao;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
