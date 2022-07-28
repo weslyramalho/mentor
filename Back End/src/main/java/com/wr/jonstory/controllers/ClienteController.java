da2be67643935c7b0dbd2b632a0ea281e0a32aaa
weslyramalho <weslyramalho@yahoo.com.br>
2022-07-28 17:37:07 -0300

    ClienteController criado

---
+++ Back End/src/main/java/com/wr/jonstory/controllers/ClienteController.java
@@ -0,0 +1,54 @@
+package com.wr.jonstory.controllers;
+
+import com.wr.jonstory.entities.Cliente;
+import com.wr.jonstory.entities.Endereco;
+import com.wr.jonstory.services.ClienteService;
+import com.wr.jonstory.services.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> findall(){
+        List<Cliente> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Cliente>> findById(@PathVariable Long id){
+        Optional<Cliente> obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> insert(@RequestBody Cliente obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable Long id, @RequestBody Cliente obj){
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+}
